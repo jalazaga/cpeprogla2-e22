da838a920a6f4cf40040d7b1d15b0b0c87680977
jalazaga <jjlazaga2@gmail.com>
2016-10-02 22:53:35 +0800

    Create hashmat.cpp

---
+++ hashmat.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include <stdlib.h>
+using namespace std;
+
+
+int main(){
+	
+	
+	int allies, enemies, diff;
+	
+	cout<<"Hashmat Allies:";
+	cin>>allies;
+	cout<<"Hashmat Enemy:";
+	cin>>enemies;
+	
+	diff = abs(allies-enemies);
+	cout<<"Difference of number: "<<diff;
+}
+Contact GitHub API Training Shop Blog About
+© 2016 GitHub, Inc. Terms Privacy Security Status Help
